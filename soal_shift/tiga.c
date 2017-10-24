62d8274283bc22ccee24ed89ef26e44a09d38562
Jonathan <joel.sembilan8@gmail.com>
2017-10-24 21:13:33 +0700

    tambah fungsi tambah

--- soal_shift/tiga.c
+++ soal_shift/tiga.c
@@ -6,10 +6,12 @@
 
 int lohan;
 int kepiting;
+int exit;
 
 void* makan(void* arg){
 	int* binatang = (int*) arg;
-	while( (lohan>=0 && lohan<=100) || (kepiting>=0 && kepiting<=100)){
+	exit=0;
+	while((lohan>=0 && lohan<=100) || (kepiting>=0 && kepiting<=100)){
 		if(*binatang == 1){		//lohan
 			sleep(10);
 			lohan-=15;
@@ -19,5 +21,24 @@ void* makan(void* arg){
 			kepiting-=10;
 		}
 	}
+	exit=1;
 	return NULL;
 }
+
+void* tambah(void* arg){
+	char temp[1000];
+	int jumlah;
+	while((lohan>=0 && lohan<=100) || (kepiting>=0 && kepiting<=100)){
+		if(exit==1)break;
+		scanf("%s",temp);
+		scanf("%d",&jumlah);
+		if(strcmp(temp,"lohan")==0){
+			lohan+=jumlah;
+		}
+		else{
+			kepiting+=jumlah;
+		}
+	}
+	return NULL;
+}
+
