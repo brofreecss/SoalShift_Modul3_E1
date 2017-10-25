43612ea2776b93889fac41a0eafdbc8cbf0efd98
Jonathan <joel.sembilan8@gmail.com>
2017-10-25 15:00:26 +0700

    add shared memory, fix bug, improve UI

--- soal_shift/soal1_hawari.c
+++ soal_shift/soal1_hawari.c
@@ -4,9 +4,10 @@
 #include <sys/shm.h>
 #include <unistd.h>
 #include <stdlib.h>
-void main()
+
+int main()
 {
-        key_t key = 1234;
+	key_t key = 1234;
 	char pilih[10],menu1[10];
 	char a[10];
 	char b[10];
@@ -14,46 +15,69 @@ void main()
 	strcpy(a,"lihat");
 	strcpy(b,"tambah");
 	strcpy(c,"exit");
-        int *stock, i, plus;
+    
+    int *stock, i, plus;
 	const char *list[6];
+
 	list[0]= "MP4A1";
 	list[1]= "PM2-V1";
 	list[2]= "SPR-3";
 	list[3]= "SS2-V5";
 	list[4]= "SPG1-V3";
 	list[5]= "MINE";
-	int  jumlah[6]= {0,1,1,2,3,4};
+
+	int shmid = shmget(key,sizeof(int)*6,IPC_CREAT | 0666);
+	int *jumlah;
+	jumlah = shmat(shmid,NULL,0);
+
+	//init value
+	for(i=0;i<6;++i)
+		jumlah[i]=1;
+
 	while(1){
-	scanf("%s", pilih);
-	if(strcmp(pilih,c)==0) exit(EXIT_FAILURE);
-	if(strcmp(pilih,a)==0){
-	for(i=0;i<6;i++)
-	{
-	if(jumlah[i]!=0){
-	printf("%s %d\n", list[i], jumlah[i]);}
-
-	}
-	}
-	if(strcmp(pilih,"tambah")==0){
-	scanf("%s %d", menu1,&plus);
-	for(i=0;i<6;i++){
-	if(strcmp(menu1,list[i])==0){
-	jumlah[i]+=plus;
-	printf("%s %d\n", list[i],jumlah[i]);
+		system("clear");
+		printf("lihat / tambah / exit\n");
+		printf("manakah pilihanmu, O Hawari?\n");
+		scanf("%s", pilih);
+		if(strcmp(pilih,c)==0) break;
+		else if(strcmp(pilih,a)==0){
+			system("clear");
+			for(i=0;i<6;i++)
+			{
+				if(jumlah[i]!=0){
+					printf("%s: %d\n", list[i], jumlah[i]);
+				}
 			}
-
+			printf("\nPress any key to continue...\n");
+			getchar();
+		}
+		else if(strcmp(pilih,"tambah")==0){
+			scanf("%s %d", menu1,&plus);
+			for(i=0;i<6;i++){
+				if(strcmp(menu1,list[i])==0){
+					jumlah[i]+=plus;
+					printf("update jumlah : %s %d\n", list[i],jumlah[i]);
+				}
+			}
+			printf("Press any key to continue...\n");
+			getchar();
+		}
+		getchar();
 	}
-}
-}
-       int shmid = shmget(key, sizeof(int), IPC_CREAT | 0666);
-        stock = shmat(shmid, NULL, 0);
 
-        *stock = 10;
+	shmdt(jumlah);
+	shmctl(shmid, IPC_RMID, NULL);
+	return 0;
 
-        printf("Program 1 : %d\n", *stock);
+	// int shmid = shmget(key, sizeof(int), IPC_CREAT | 0666);
+	// stock = shmat(shmid, NULL, 0);
 
-        sleep(5);
-        printf("Program 1: %d\n", *stock);
-        shmdt(stock);
-     	shmctl(shmid, IPC_RMID, NULL);
+	// *stock = 10;
+
+	// printf("Program 1 : %d\n", *stock);
+
+	// sleep(5);
+	// printf("Program 1: %d\n", *stock);
+	// shmdt(stock);
+	// shmctl(shmid, IPC_RMID, NULL);
 }
--- soal_shift/soal1_pembeli.c
+++ soal_shift/soal1_pembeli.c
@@ -4,13 +4,11 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
-void main()
+
+int main()
 {
-//        key_t key = 1234;
-  //      int *value;
-	
-      //  int shmid = shmget(key, sizeof(int), IPC_CREAT | 0666);
-   //     value = shmat(shmid, NULL, 0);
+    key_t key = 1234;
+
 	char pilih[10],menu1[10];
 	char a[10];
 	char b[10];
@@ -18,7 +16,8 @@ void main()
 	strcpy(a,"lihat");
 	strcpy(b,"beli");
 	strcpy(c,"exit");
-        int *stock, i, kurang;
+    
+    int *stock, i, kurang;
 	const char *list[6];
 	list[0]= "MP4A1";
 	list[1]= "PM2-V1";
@@ -26,27 +25,47 @@ void main()
 	list[3]= "SS2-V5";
 	list[4]= "SPG1-V3";
 	list[5]= "MINE";
-	int  jumlah[6]= {1,1,1,2,3,4};
-	while(1){
-	scanf("%s", pilih);
-	if(strcmp(pilih,c)==0) exit(EXIT_FAILURE);
-	if(strcmp(pilih,a)==0){
-	for(i=0;i<6;i++)
-	{
-	printf("%s %d\n", list[i], jumlah[i]);
-	}
-	}
-	if(strcmp(pilih,"beli")==0){
-	scanf("%s %d", menu1, &kurang);
-	for(i=0;i<6;i++){
-	if(strcmp(menu1,list[i])==0){
-	jumlah[i]-=kurang;
-	printf("%s %d\n", list[i],jumlah[i]);
-			}
 
+	int shmid = shmget(key,sizeof(int)*6,IPC_CREAT | 0666);
+	int *jumlah;
+	jumlah = shmat(shmid,NULL,0);
+
+	//init value
+	for(i=0;i<6;++i)
+		jumlah[i]=1;
+
+	while(1){
+		system("clear");
+		printf("lihat / beli / exit\n");
+		printf("manakah pilihanmu, pembeli?\n");
+		scanf("%s", pilih);
+		if(strcmp(pilih,c)==0) break;
+		else if(strcmp(pilih,a)==0){
+			for(i=0;i<6;i++)
+			{
+				printf("%s %d\n", list[i], jumlah[i]);
+			}
+			printf("\nPress any key to continue...\n");
+			getchar();
+		}
+
+		else if(strcmp(pilih,b)==0){
+			scanf("%s %d", menu1, &kurang);
+			for(i=0;i<6;i++){
+				if(strcmp(menu1,list[i])==0){
+					jumlah[i]-=kurang;
+					printf("Barang %s terbeli %d buah\n", list[i],jumlah[i]);
+				}
+			}
+			printf("\nPress any key to continue...\n");
+			getchar();
+		}
+		getchar();
 	}
-}
-}
+	shmdt(jumlah);
+	shmctl(shmid, IPC_RMID, NULL);
+
+	return 0;
  //       printf("Program 1 : %d\n", *value);
 //	*value = 30;
 		
