08213058c9044ff8f07e1c924a4c253f7663c8fd
trus25 <fzuhdi50@gmail.com>
2017-10-25 14:07:58 +0700

    perubahan

--- soal_shift/soal1_hawari.c
+++ soal_shift/soal1_hawari.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
+#include <string.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <unistd.h>
-#include <string.h>
+#include <stdlib.h>
 void main()
 {
         key_t key = 1234;
-	char menu1,pilih;
+	char pilih[10],menu1[10];
 	char a[10];
 	char b[10];
+	char c[10];
 	strcpy(a,"lihat");
 	strcpy(b,"tambah");
+	strcpy(c,"exit");
         int *stock, i, plus;
 	const char *list[6];
 	list[0]= "MP4A1";
@@ -20,31 +23,37 @@ void main()
 	list[4]= "SPG1-V3";
 	list[5]= "MINE";
 	int  jumlah[6]= {0,1,1,2,3,4};
-	fgets(pilih, sizeof(pilih),stdin);
+	while(1){
+	scanf("%s", pilih);
+	if(strcmp(pilih,c)==0) exit(EXIT_FAILURE);
 	if(strcmp(pilih,a)==0){
 	for(i=0;i<6;i++)
 	{
 	if(jumlah[i]!=0){
-	printf("%s %d\n", list [i], jumlah[i]);}
+	printf("%s %d\n", list[i], jumlah[i]);}
+
 	}
 	}
-//	if(strcmp(pilih,"tambah")==0){
-//	scanf("%s %d", menu1,&plus);
-//	for(i=0;i<6;i++){
-//	if(menu1=='list[i]'){
-//	list[i]+=plus;}
-//	}
-//	}
-       // int shmid = shmget(key, sizeof(int), IPC_CREAT | 0666);
-       // stock = shmat(shmid, NULL, 0);
+	if(strcmp(pilih,"tambah")==0){
+	scanf("%s %d", menu1,&plus);
+	for(i=0;i<6;i++){
+	if(strcmp(menu1,list[i])==0){
+	jumlah[i]+=plus;
+	printf("%s %d\n", list[i],jumlah[i]);
+			}
 
-       // *stock = 10;
+	}
+}
+}
+       int shmid = shmget(key, sizeof(int), IPC_CREAT | 0666);
+        stock = shmat(shmid, NULL, 0);
 
-       // printf("Program 1 : %d\n", *stock);
+        *stock = 10;
 
-       // sleep(5);
+        printf("Program 1 : %d\n", *stock);
 
-       // printf("Program 1: %d\n", *stock);
-       // shmdt(stock);
-        //shmctl(shmid, IPC_RMID, NULL);
+        sleep(5);
+        printf("Program 1: %d\n", *stock);
+        shmdt(stock);
+     	shmctl(shmid, IPC_RMID, NULL);
 }
